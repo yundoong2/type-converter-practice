f6c07c2b5b6b7620503abde2922f6a87b82a320f
YHCHOI <cyh6809@gmail.com>
2023-04-26 16:08:40 +0900

    Converter 및 Formatter 실습

--- src/main/java/hello/typeconverter/TypeConverterApplication.java
+++ src/main/java/hello/typeconverter/TypeConverterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TypeconverterApplication {
+public class TypeConverterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TypeconverterApplication.class, args);
+		SpringApplication.run(TypeConverterApplication.class, args);
 	}
 
 }
--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,7 +1,9 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,19 @@ public class HelloController {
         Integer intValue = Integer.valueOf(data);
         System.out.println("intValue = " + intValue);
 
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
         return "ok";
     }
 }
